7b7c23c2181d8838d9dc63ec8a9738a7f41df6ad
Omkar Bhong <omkarbhong999@gmail.com>
2026-02-19 11:52:27 +0530

    fixed

---
+++ index.js
@@ -0,0 +1,11 @@
+// Wait until page loads
+document.addEventListener("DOMContentLoaded", function () {
+
+    const button = document.getElementById("myButton");
+    const message = document.getElementById("message");
+
+    button.addEventListener("click", function () {
+        message.innerHTML = "🎉 Button clicked successfully!  wow yeah ";
+    });
+
+});
